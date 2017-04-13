da276872a29df51182a9da8e1720763d9d5b583d
郑国榕 <zhengguorong@bluemoon.com.cn>
2017-04-13 11:36:45 +0800

    init

---
+++ config/config.default.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  // should change to your own
+  config.keys = appInfo.name + '_1492054139103_7031';
+
+  return config;
+};
