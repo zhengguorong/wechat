4bde43f1baa0a5df6f18efb00835f961769f0178
郑国榕 <zhengguorong@bluemoon.com.cn>
2017-04-19 17:16:45 +0800

    修复获取openid失败问题

--- app/controller/api/wechat.js
+++ app/controller/api/wechat.js
@@ -7,7 +7,7 @@ module.exports = app => {
       const code = this.ctx.params.code;
       const url = `https://api.weixin.qq.com/sns/jscode2session?appid=${app.config.appId}&secret=${app.config.appSecret}&js_code=${code}&grant_type=authorization_code`;
       const result = yield this.ctx.curl(url, { dataType: 'json' });
-      const openId = result.data.openId;
+      const openId = result.data.openid;
       if (openId) {
         const hasRegister = yield this.ctx.service.user.hasRegister(openId);
         if (!hasRegister) {
