624776f3167fbb86fa120eee2626995be2b331cb
郑国榕 <zhengguorong@bluemoon.com.cn>
2017-04-19 16:30:37 +0800

    修改获取openid方法

--- app/controller/api/wechat.js
+++ app/controller/api/wechat.js
@@ -5,7 +5,7 @@ module.exports = app => {
     * getOpenId() {
       const code = this.ctx.params.code;
       const url = `https://api.weixin.qq.com/sns/jscode2session?appid=${app.config.appId}&secret=${app.config.appSecret}&js_code=${code}&grant_type=authorization_code`;
-      const result = yield this.ctx.curl(url);
+      const result = yield this.ctx.curl(url, { dataType: 'json' });
       this.ctx.status = result.status;
       this.ctx.body = result.body;
     }
